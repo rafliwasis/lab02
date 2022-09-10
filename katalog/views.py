d8dc4f29eeebe2111d67b7b6f270349681645211
rafli.wasis <rafliwasisanggito@gmail.com>
2022-09-10 20:46:51 +0700

    komit tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Rafli Wasis Anggito',
+    'npm' : '2106751442'
+    }
+    return render(request, "katalog.html", context)
+
